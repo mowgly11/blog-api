1c07de97bff8b7f1b7cc80fbffa3a2299521709f
mowgly11 <oussamabouzalim55@gmail.com>
2025-07-21 18:59:03 +0100

    C:/Program Files/Git/hide_blog endpoint in order to change visibility of a blog

---
+++ backend/routes/POST/unhideBlog.js
@@ -0,0 +1,19 @@
+import database from "../../database/blogsCollectionActions.js";
+import utils from "../../utils/responseModel.js";
+import Middleware from "../../middleware/middleware.js";
+
+export default {
+    methods: ["patch"],
+    endpoint: "/unhide_blog",
+    middleware: [Middleware.requireJSONContent, Middleware.checkAuthenticated],
+    Patch: async function (req, res, next) {
+        let id = req.body.id;
+    
+        if(!id || id.trim() === "") return res.status(400).json(utils.getResponseVariables(400, 'Missing required fields (id)'));
+
+        let blogVisible = await database.changeBlogVisibility(id, true);
+        if(!blogVisible) return res.status(500).json(utils.getResponseVariables(500, 'Internal Server Error'));
+
+        res.json(utils.getResponseVariables(200));
+    },
+};
