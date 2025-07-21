009a06b09c14f3e83187430302c728e8cf6baf51
mowgly11 <oussamabouzalim55@gmail.com>
2025-07-21 18:34:12 +0100

    added JWT token middleware verification for protected routes

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -1,9 +1,28 @@
-import Utils from "../utils/utils.js";
+import logger from "../utils/logger.js";
+import Utils from "../utils/responseModel.js";
+import jwt from 'jsonwebtoken';
+import database from '../database/authCollectionActions.js';
 
 class Middleware {
     static requireJSONContent(req, res, next) {
-        if(!req.is('application/json')) return res.status(415).json(Utils.getResponseVariables(400, 'Only JSON content is allowed on this endpoint', null));
+        if (!req.is('application/json')) return res.status(415).json(Utils.getResponseVariables(400, 'Only JSON content is allowed on this endpoint', null));
         next();
+    }
+
+    static checkAuthenticated(req, res, next) {
+        const cookie = req.cookies.auth_token;
+        if (!cookie || cookie == 'undefined') return res.json(Utils.getResponseVariables(403, 'Invalid Authorization'))
+        jwt.verify(cookie, process.env.JWT_SECRET, async (err, user) => {
+            if (err) {
+                res.json(Utils.getResponseVariables(403, 'Invalid Authorization'));
+                return logger.error("middleware error when validating jwt token: " + err);
+            }
+
+            let userProfile = await database.findUserById(user.id);
+            if (!userProfile) return res.json(Utils.getResponseVariables(403, 'Invalid Authorization'));
+            req.user = userProfile;
+            next();
+        });
     }
 }
 
