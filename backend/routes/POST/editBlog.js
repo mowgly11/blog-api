e74561c4a0a5b298f7a62d41608b18a3b1b78fd0
mowgly11 <oussamabouzalim55@gmail.com>
2025-07-21 18:58:32 +0100

    added input sanitization and better respones

--- backend/routes/POST/editBlog.js
+++ backend/routes/POST/editBlog.js
@@ -1,21 +1,36 @@
-import database from "../../database/databaseActions.js";
-import utils from "../../utils/utils.js";
+import database from "../../database/blogsCollectionActions.js";
+import utils from "../../utils/responseModel.js";
+import Middleware from "../../middleware/middleware.js";
+import sanitize from "sanitize-html";
 
 export default {
-  methods: ["post"],
+  methods: ["patch"],
   endpoint: "/edit_blog",
-  Post: async function (req, res, next) {
-    if (!req.body.id || !req.body.props || Object.keys(req.body.props).length === 0) 
-      return res.json(utils.getResponseVariables(400, "Bad Request"));
+  middleware: [Middleware.requireJSONContent, Middleware.checkAuthenticated],
+  Patch: async function (req, res, next) {
+    let { id, title, author, content } = req.body;
 
-    const blogToModify = await database.update(req.body.id, req.body.props);
-    if(!blogToModify) return res.json(utils.getResponseVariables(502, "Database Error/Blog not found"));
+    if (!id || !title || !author || !content)
+      return res.status(400).json(utils.getResponseVariables(400, 'Missing required fields (id, title, author, content)', null));
+
+    title = sanitize(title).trim();
+    author = sanitize(author).trim();
+    content = sanitize(content, {
+      allowedTags: ['b', 'i', 'string', 'a', 'code', 'table', 'thead', 'tbody', 'tr', 'th', 'td', 'em', 'img'],
+      allowedAttributes: {
+        'a': ['href'],
+        'img': ['src', 'alt']
+      }
+    }).trim();
+
+    if (title === "" || author === "" || content === "")
+      return res.json(utils.status(400).getResponseVariables(400, "invalid/empty fields", null));
+
+    const blogToModify = await database.update(id, { title, author, content });
+    if (!blogToModify) return res.json(utils.getResponseVariables(404, "Blog not found"));
 
     res.json(
-      utils.getResponseVariables(200, null, {
-        before: blogToModify.beforeModifictions,
-        after: blogToModify.foundBlog,
-      })
+      utils.getResponseVariables(200, null, blogToModify)
     );
   },
 };
