e09390f9903f793d88abadf5fd413ffc503d4af0
mowgly11 <oussamabouzalim55@gmail.com>
2025-07-21 18:59:38 +0100

    added JWT authentication for api managers

---
+++ backend/routes/POST/login.js
@@ -0,0 +1,32 @@
+import sanitize from "sanitize-html";
+import database from "../../database/authCollectionActions.js";
+import Middleware from "../../middleware/middleware.js";
+import utils from "../../utils/responseModel.js";
+import jwt from "jsonwebtoken";
+
+export default {
+    methods: ["post"],
+    endpoint: "/login",
+    middleware: [Middleware.requireJSONContent],
+    Post: async function (req, res, next) {
+        let username = req.body.username;
+        let password = req.body.password;
+
+        if (!username || !password || sanitize(username) === "" || sanitize(password) === "") return res.json(utils.getResponseVariables(401, 'Invalid username or password', null));
+
+        let userAllowed = await database.validateCredentials(username, password);
+
+        if (!userAllowed) return res.json(utils.getResponseVariables(401, 'Invalid username or password', null));
+
+        let token = jwt.sign({ id: userAllowed.id }, process.env.JWT_SECRET, { expiresIn: '1 day' });
+
+        res.cookie('auth_token', token, {
+            httpOnly: true,
+            sameSite: 'None',
+            secure: true,
+            maxAge: 1000 * 60 * 60 * 24
+        });
+
+        res.status(200).json(utils.getResponseVariables(200));
+    },
+};
