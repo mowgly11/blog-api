a1dc813e97ec1d38abf433d9568fb317ffe8a10c
mowgly11 <oussamabouzalim55@gmail.com>
2025-07-21 02:36:19 +0100

    changed query params to set a limit of how many documents you can pull and improved security

--- backend/routes/GET/getBlogs.js
+++ backend/routes/GET/getBlogs.js
@@ -1,14 +1,19 @@
 import database from "../../database/databaseActions.js";
+import Utils from '../../utils/utils.js';
 
 export default {
   methods: ["get"],
-  endpoint: "/get_blogs",
+  endpoint: "/get_blogs?",
   Get: async function (req, res, next) {
-    const allBlogs = await database.findMultiple(0);
-    res.json({
-      status: 200,
-      error: null,
-      data: { blogs: allBlogs, length: allBlogs.length },
-    });
+    let from = req.query.from;
+    let to = req.query.to;
+    if (!from || !to) return res.json(Utils.getResponseVariables(400, 'missing query params (from, to)', null));
+    if (isNaN(from) || isNaN(to)) return res.json(Utils.getResponseVariables(400, 'from and to need to be integers', null));
+    from = parseInt(from);
+    to = parseInt(to);
+    if (from > to || from < 0 || to < 0 || to - from > 10) return res.json(Utils.getResponseVariables(400, 'from and to need to be positive integers and to value needs to be greater than from', null))
+
+    const allBlogs = await database.findMultiple(from, to);
+    res.json(Utils.getResponseVariables(200, null, { blogs: allBlogs, length: allBlogs.length }));
   },
 };
