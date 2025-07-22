c40fae6833bff72ee503203fcb926dca1d581975
mowgly11 <oussamabouzalim55@gmail.com>
2025-07-22 19:25:00 +0100

    added login attempts logging

--- backend/routes/POST/login.js
+++ backend/routes/POST/login.js
@@ -3,6 +3,8 @@ import database from "../../database/authCollectionActions.js";
 import Middleware from "../../middleware/middleware.js";
 import utils from "../../utils/responseModel.js";
 import jwt from "jsonwebtoken";
+import logger from "../../utils/logger.js";
+import requestIP from "request-ip";
 
 export default {
     methods: ["post"],
@@ -16,17 +18,22 @@ export default {
 
         let userAllowed = await database.validateCredentials(username, password);
 
-        if (!userAllowed) return res.json(utils.getResponseVariables(401, 'Invalid username or password', null));
-
+        if (!userAllowed) {
+            logger.error(`a failed logging attempt was initiated with these credentials: ${username} - ${password} | IP: ${requestIP.getClientIp(req)}`);
+            return res.json(utils.getResponseVariables(401, 'Invalid username or password', null))
+        };
+        
+        
         let token = jwt.sign({ id: userAllowed.id }, process.env.JWT_SECRET, { expiresIn: '1 day' });
-
+        
         res.cookie('auth_token', token, {
             httpOnly: true,
             sameSite: 'None',
             secure: true,
             maxAge: 1000 * 60 * 60 * 24
         });
-
+        
+        logger.info(`a successful logging attempt was initiated with these credentials: ${username} - ${password} | IP: ${requestIP.getClientIp(req)}`);
         res.status(200).json(utils.getResponseVariables(200));
     },
 };
