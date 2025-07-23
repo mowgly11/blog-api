24084b0dd476b214c1b2a626dba712ab2e5a5bba
mowgly11 <oussamabouzalim55@gmail.com>
2025-07-23 13:16:11 +0100

    fixed a double import typo

--- backend/routes/GET/getPublicBlogs.js
+++ backend/routes/GET/getPublicBlogs.js
@@ -1,6 +1,5 @@
 import database from "../../database/blogsCollectionActions.js";
 import utils from "../../utils/responseModel.js";
-import Utils from '../../utils/responseModel.js';
 
 export default {
   methods: ["get"],
@@ -8,14 +7,14 @@ export default {
   Get: async function (req, res, next) {
     let from = req.query.from;
     let to = req.query.to;
-    if (!from || !to) return res.status(400).json(Utils.getResponseVariables(400, 'missing query params (from, to)', null));
-    if (isNaN(from) || isNaN(to)) return res.status(400).json(Utils.getResponseVariables(400, 'from and to need to be integers', null));
+    if (!from || !to) return res.status(400).json(utils.getResponseVariables(400, 'missing query params (from, to)', null));
+    if (isNaN(from) || isNaN(to)) return res.status(400).json(utils.getResponseVariables(400, 'from and to need to be integers', null));
     from = parseInt(from);
     to = parseInt(to);
-    if (from > to || from < 0 || to < 0) return res.status(400).json(Utils.getResponseVariables(400, 'from and to need to be positive integers and to value needs to be greater than from', null))
+    if (from > to || from < 0 || to < 0) return res.status(400).json(utils.getResponseVariables(400, 'from and to need to be positive integers and to value needs to be greater than from', null))
     if (to - from > process.env.MAX_BLOGS) return res.status(400).json(utils.getResponseVariables(400, 'you can only grab a maximum of ' + process.env.MAX_BLOGS + 'blogs at a time'));
     
     const allBlogs = await database.findMultiple(from, to, true);
-    res.json(Utils.getResponseVariables(200, null, { blogs: allBlogs, length: allBlogs.length }));
+    res.json(utils.getResponseVariables(200, null, { blogs: allBlogs, length: allBlogs.length }));
   },
 };
