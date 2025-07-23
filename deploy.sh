9db449f53207a8d767b45b94c0ee815a1efa714c
mowgly11 <oussamabouzalim55@gmail.com>
2025-07-23 13:09:50 +0100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,11 @@ echo "deleting previous pm2 instance.."
 pm2 stop index
 pm2 delete index
 
+echo "Updading dependencies"
+
+npm audit
+npm update
+
 echo "creating a new pm2 instance"
 
 pm2 start backend/index.js
